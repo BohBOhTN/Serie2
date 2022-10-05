5bb4cbd8b250a0f9063599db5a6b233381c73647
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2022-10-05 22:09:41 +0200

    Update Exercice2.c
    quelques fautes d'orthographe au niveau de Printf

--- Exercice2.c
+++ Exercice2.c
@@ -11,12 +11,12 @@ int main()
     printf("Donner le troisieme  valeur \n");
     scanf("%d",&num3);
     if(num1<num2 && num1<num3){
-        printf("Le valeur [%d] est le plus petit entre les autres valeurs",num1);
+        printf("La valeur [%d] est le plus petit entre les autres valeurs",num1);
     }else{
         if(num2<num1 && num2<num3){
-            printf("Le valeur [%d] est le plus petit entre les autres valeurs",num2);
+            printf("La valeur [%d] est le plus petit entre les autres valeurs",num2);
         }else{
-            printf("Le valeur [%d] est le plus petit entre les autres valeurs",num3);
+            printf("La valeur [%d] est le plus petit entre les autres valeurs",num3);
         }
     }
     return 0;
